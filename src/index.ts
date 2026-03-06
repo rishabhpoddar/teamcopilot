4f633202d9540f0287fa59209d8083e9b4ae8486
rishabhpoddar <rishabh.poddar@gmail.com>
2026-03-06 19:21:37 +0530

    fixes build

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
-import cors from "cors";
 import { logError } from "./logging";
 import authRouter from "./auth";
 import workflowsRouter from "./workflows";
