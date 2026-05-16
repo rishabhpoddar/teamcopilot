db7aadfce3a0e776a2efe9485c1524ba96fba51b
rishabhpoddar <rishabh.poddar@gmail.com>
2026-05-16 14:08:55 +0530

    wip

--- src/types/cronjob.ts
+++ src/types/cronjob.ts
@@ -1,5 +1,7 @@
 export type CronjobTargetType = "prompt" | "workflow";
 
+export type CronjobMonitorTimeoutUnit = "minutes" | "hours" | "days";
+
 export type CronjobSchedule = {
     cron_expression: string;
     timezone: string;
