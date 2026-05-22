df86da0cc29b6a2fde9f868ebbf31ba4c94e7ad4
rishabhpoddar <rishabh.poddar@gmail.com>
2026-05-22 12:01:14 +0530

    increases limit of file save

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ export function createApp(): express.Express {
     const app = express();
     const frontendDistDirectory = getFrontendDistDirectory();
 
-    app.use(express.json());
+    app.use(express.json({ limit: "10mb" }));
 
     // Logging middleware
     // app.use((req, res, next) => {
