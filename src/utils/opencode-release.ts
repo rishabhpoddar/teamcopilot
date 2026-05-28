53c8f232cb1eb09ff97a7b4768307cea216708b9
rishabhpoddar <rishabh.poddar@gmail.com>
2026-05-28 12:15:23 +0530

    more fixes

--- src/utils/opencode-release.ts
+++ src/utils/opencode-release.ts
@@ -1,4 +1,4 @@
-const OPENCODE_RELEASE_TAG = "teamcopilot-1.3.7-changes-202605280620";
+const OPENCODE_RELEASE_TAG = "teamcopilot-1.3.7-changes-202605280640";
 
 const OPENCODE_RELEASE_BASE_URL = `https://github.com/rishabhpoddar/opencode/releases/download/${OPENCODE_RELEASE_TAG}`;
 
